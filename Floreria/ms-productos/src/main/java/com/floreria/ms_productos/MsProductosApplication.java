d587d1760792f31121b5f7b3951c89979db24ff5
Sebastian Olivares <sebastian.olivarest@hotmail.com>
2026-07-10 16:45:04 -0400

    Corrección Docker compose 1

--- Floreria/ms-productos/src/main/java/com/floreria/ms_productos/MsProductosApplication.java
+++ Floreria/ms-productos/src/main/java/com/floreria/ms_productos/MsProductosApplication.java
@@ -1,9 +1,14 @@
 package com.floreria.ms_productos;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {
+		@Server(url = "http://localhost:8081", description = "Ruta a través del Gateway")
+})
 public class MsProductosApplication {
 
 	public static void main(String[] args) {
